eb9d2271c7c8bec22a65c7eb9834718f6dc58691
Marcin Tatoń <tatonmarcinpr@gmail.com>
2022-09-24 01:37:13 +0200

    Reverted

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -48,7 +48,7 @@ module.exports = {
     ],
   },
   output: {
-    path: path.resolve(__dirname, '..', './dist'),
+    path: path.resolve(__dirname, '..', './build'),
     filename: '[name].bundle.js',
   },
   optimization: {
